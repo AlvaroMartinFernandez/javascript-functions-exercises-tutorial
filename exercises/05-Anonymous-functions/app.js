dc49c17c91f0b7ce79446954a761edd6e485a3fd
Alvaro Martin Fernandez <91843474+AlvaroMartinFernandez@users.noreply.github.com>
2023-04-20 21:09:07 +0000

    Finalizado

--- exercises/05-Anonymous-functions/app.js
+++ exercises/05-Anonymous-functions/app.js
@@ -1,6 +1,4 @@
 let multy = function(a,b){
     return a*b;
 }
-//Dont edit anything ABOVE this line
-
-//your code here
+console.log(multy(324234,47));
--- exercises/08-Function-parameters/app.js
+++ exercises/08-Function-parameters/app.js
@@ -1,5 +1,8 @@
 // Your code goes here:
-
+ function renderPerson (nombre,fecha,color,edad,sexo)
+ {
+  return (nombre +" is a "+ edad +" years old "+ sexo +" born in "+fecha+ " with "+color+ " eyes");
+ }
 
 
   ///*** Do not edit below this line ****/
